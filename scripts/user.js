290d22ff917ed75b4d955db290c64e17d1b4a4ec
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2020-02-20 18:42:22 -0500

    ClubProgramKahootInicioFirebase

--- scripts/user.js
+++ scripts/user.js
@@ -1,3 +1,11 @@
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
 var roomNumberCont = document.querySelector('.roomNumberCont');
 var userNameCont = document.querySelector('.userNameCont');
 var messageCont = document.querySelector('.messageCont');
@@ -7,7 +15,7 @@ function handleRoomNumberPanelSubmit (event) {
     event.preventDefault();
     var roomNumberInput = roomNumberCont.querySelector('input');
     var roomNumber = roomNumberInput.value;
-
+    
     roomNumberCont.classList.add('hidden');
     userNameCont.classList.remove('hidden');
     console.log('test', roomNumber);
